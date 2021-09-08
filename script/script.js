33c36d193f59514a152761acf0e8af3b962bb85a
olga2710 <olga.liaz@yandex.ru>
2021-09-08 16:51:51 +0300

    GIT

--- script/script.js
+++ script/script.js
@@ -4,12 +4,27 @@ document.addEventListener('DOMContentLoaded',()=>{
         btnOpenModal = document.querySelector("#btnOpenModal"),
         modal = document.querySelector('#modalBlock'),
         closeModal = document.querySelector('#closeModal'),
-        questionTitle = document.querySelector('#guestion'),
-        
+        questionTitle = document.querySelector('#question'),
+        formAnswers = document.querySelector('#formAnswers'),
         
         playTest=()=>{
             const renderQuistion = ()=>{
-                questionTitle.textContent = 'Какого цвета бургур Вы хотите?'
+                questionTitle.textContent = 'Какого цвета бургер Вы хотите?'
+
+                formAnswers.innerHTML= `
+                <div class="answers-item d-flex flex-column">
+                    <input type="radio" id="answerItem1" name="answer" class="d-none">
+                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src="./image/burger.png" alt="burger">
+                    <span>Стандарт</span>
+                    </label>
+                </div>
+                <div class="answers-item d-flex justify-content-center">
+                    <input type="radio" id="answerItem2" name="answer" class="d-none">
+                    <label for="answerItem2" class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
+                    <span>Черный</span>
+                    </label>`
 
             };
             renderQuistion();
