6439d75168536d652d498dc1e81f8da9349d10fa
olga2710 <olga.liaz@yandex.ru>
2021-09-08 16:59:03 +0300

    GIT

--- script/script.js
+++ script/script.js
@@ -6,6 +6,14 @@ document.addEventListener('DOMContentLoaded',()=>{
         closeModal = document.querySelector('#closeModal'),
         questionTitle = document.querySelector('#question'),
         formAnswers = document.querySelector('#formAnswers'),
+        standartBurger = {
+            name: 'Стандарт',
+            img: './image/burger.png',
+        },
+        blackBurger = {
+            name: 'Черный',
+            img: './image/burgerBlack.png',
+        }
         
         playTest=()=>{
             const renderQuistion = ()=>{
@@ -15,15 +23,15 @@ document.addEventListener('DOMContentLoaded',()=>{
                 <div class="answers-item d-flex flex-column">
                     <input type="radio" id="answerItem1" name="answer" class="d-none">
                     <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burger.png" alt="burger">
-                    <span>Стандарт</span>
+                    <img class="answerImg" src="${standartBurger.img}" alt="burger">
+                    <span>${standartBurger.name}</span>
                     </label>
                 </div>
                 <div class="answers-item d-flex justify-content-center">
                     <input type="radio" id="answerItem2" name="answer" class="d-none">
                     <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
-                    <span>Черный</span>
+                    <img class="answerImg" src="${blackBurger.img}" alt="burger">
+                    <span>${blackBurger.name}</span>
                     </label>`
 
             };
