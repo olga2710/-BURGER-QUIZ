be953eeaac482826bde2d94ebd54895f8f0ae705
olga2710 <olga.liaz@yandex.ru>
2021-09-08 16:46:52 +0300

    git

--- script/script.js
+++ script/script.js
@@ -3,11 +3,23 @@ document.addEventListener('DOMContentLoaded',()=>{
     const 
         btnOpenModal = document.querySelector("#btnOpenModal"),
         modal = document.querySelector('#modalBlock'),
-        closeModal = document.querySelector('#closeModal');
+        closeModal = document.querySelector('#closeModal'),
+        questionTitle = document.querySelector('#guestion'),
+        
+        
+        playTest=()=>{
+            const renderQuistion = ()=>{
+                questionTitle.textContent = 'Какого цвета бургур Вы хотите?'
+
+            };
+            renderQuistion();
+
+        };
 
 
     btnOpenModal.addEventListener('click', ()=>{
         modal.classList.add('d-block');  
+        playTest();
     });
 
     closeModal.addEventListener('click', ()=>{
