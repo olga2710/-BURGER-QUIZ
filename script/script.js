cd4b341b33027ead531560e5c505919ee790db43
olga2710 <olga.liaz@yandex.ru>
2021-09-08 16:30:27 +0300

    git

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded',()=>{
     'use strict';
     const 
-        btnOpenModal = document.querySelector("#btnOpenModal");
+        btnOpenModal = document.querySelector("#btnOpenModal"),
+        modal = document.querySelector('#modalBlock');
 
 
-    btnOpenModal.addEventListener('click', ()=>{ 
+    btnOpenModal.addEventListener('click', ()=>{
+        modal.classList.add('d-block'); 
     
 })
 })
