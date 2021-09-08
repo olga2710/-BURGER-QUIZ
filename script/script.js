b5258e40577bc88adf941e626f585d235bd5fe65
olga2710 <olga.liaz@yandex.ru>
2021-09-08 17:00:16 +0300

    gIt

--- script/script.js
+++ script/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
         
         playTest=()=>{
-            const renderQuistion = ()=>{
+            const renderQuistion = ()=>{ 
                 questionTitle.textContent = 'Какого цвета бургер Вы хотите?'
 
                 formAnswers.innerHTML= `
