33596b46d3076ce17bf197ca41a0ff21b5ffa346
olga2710 <olga.liaz@yandex.ru>
2021-09-09 15:09:18 +0300

    new Version

--- script/script.js
+++ script/script.js
@@ -6,38 +6,153 @@ document.addEventListener('DOMContentLoaded',()=>{
         closeModal = document.querySelector('#closeModal'),
         questionTitle = document.querySelector('#question'),
         formAnswers = document.querySelector('#formAnswers'),
-        standartBurger = {
-            name: 'Стандарт',
-            img: './image/burger.png',
-        },
-        blackBurger = {
-            name: 'Черный',
-            img: './image/burgerBlack.png',
-        }
+        nextBtn = document.querySelector('#next'),
+        prevBtn = document.querySelector('#prev'),
+        sendBtn = document.querySelector('#send'),
+        questions = [
+            {
+                question: "Какого цвета бургер?",
+                answers: [
+                    {
+                        name: 'Стандарт',
+                        img: './image/burger.png'
+                    },
+                    {
+                        name: 'Черный',
+                        img: './image/burgerBlack.png'
+                    }
+                ],
+                type: 'radio'
+            },
+            {
+                question: "Из какого мяса котлета?",
+                answers: [
+                    {
+                        name: 'Курица',
+                        img: './image/chickenMeat.png'
+                    },
+                    {
+                        name: 'Говядина',
+                        img: './image/beefMeat.png'
+                    },
+                    {
+                        name: 'Свинина',
+                        img: './image/porkMeat.png'
+                    }
+                ],
+                type: 'radio'
+            },
+            {
+                question: "Дополнительные ингредиенты?",
+                answers: [
+                    {
+                        name: 'Помидор',
+                        img: './image/tomato.png'
+                    },
+                    {
+                        name: 'Огурец',
+                        img: './image/cucumber.png'
+                    },
+                    {
+                        name: 'Салат',
+                        img: './image/salad.png'
+                    },
+                    {
+                        name: 'Лук',
+                        img: './image/onion.png'
+                    }
+                ],
+                type: 'checkbox'
+            },
+            {
+                question: "Добавить соус?",
+                answers: [
+                    {
+                        name: 'Чесночный',
+                        img: './image/sauce1.png'
+                    },
+                    {
+                        name: 'Томатный',
+                        img: './image/sauce2.png'
+                    },
+                    {
+                        name: 'Горчичный',
+                        img: './image/sauce3.png'
+                    }
+                ],
+                type: 'radio'
+            }
+        ],
         
         playTest=()=>{
-            const renderQuistion = ()=>{ 
-                questionTitle.textContent = 'Какого цвета бургер Вы хотите?'
-
-                formAnswers.innerHTML= `
-                <div class="answers-item d-flex flex-column">
-                    <input type="radio" id="answerItem1" name="answer" class="d-none">
-                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="${standartBurger.img}" alt="burger">
-                    <span>${standartBurger.name}</span>
+            let indexQuestion =0;
+            const renderAnswers =(i) =>{
+                questions[i].answers.forEach((answer )=>{
+                    const answerItem = document.createElement('div');
+                    answerItem.classList.add('answers-item','d-flex', 'flex-column');
+                    answerItem.innerHTML= `
+                    <input type="${questions[i].type}" id="${answer.name}" name="answer" class="d-none">
+                    <label for="${answer.name}" class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src="${answer.img}" alt="burger">
+                    <span>${answer.name}</span>
                     </label>
-                </div>
-                <div class="answers-item d-flex justify-content-center">
-                    <input type="radio" id="answerItem2" name="answer" class="d-none">
-                    <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="${blackBurger.img}" alt="burger">
-                    <span>${blackBurger.name}</span>
-                    </label>`
+                    `;
+                    formAnswers.appendChild(answerItem);
+                   
+                }
+            )};
+            const renderQuistion = (i)=>{ 
+                questionTitle.textContent = `${questions[i].question}`;
+                renderAnswers(i);
 
             };
-            renderQuistion();
+            renderQuistion(indexQuestion);
 
-        };
+            const disablePrevBtn=()=>{
+            if(indexQuestion<1){
+                prevBtn.classList.add('d-none')
+            } else {
+                prevBtn.classList.remove('d-none')
+            };};
+
+            const disableNextBtn=()=>{
+                if (indexQuestion==(questions.length-1)){
+                    nextBtn.classList.add('d-none');
+                    sendBtn.classList.remove('d-none');
+                } else {
+                    nextBtn.classList.remove('d-none');
+                    sendBtn.classList.add('d-none');
+
+                }
+            };
+            
+            disablePrevBtn();
+            disableNextBtn();
+
+
+            nextBtn.addEventListener('click', ()=>{
+                clearModal();
+                indexQuestion++;
+                disablePrevBtn();
+                disableNextBtn();
+                renderQuistion(indexQuestion);
+
+            });
+
+            prevBtn.addEventListener('click', ()=>{
+                clearModal();
+                indexQuestion--;
+                disablePrevBtn();
+                disableNextBtn();
+                renderQuistion(indexQuestion);
+
+
+            });
+        },
+        clearModal = ()=>{
+            formAnswers.innerHTML='';
+            questionTitle.textContent = ``;
+        }
 
 
     btnOpenModal.addEventListener('click', ()=>{
@@ -47,5 +162,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     closeModal.addEventListener('click', ()=>{
         modal.classList.remove('d-block'); 
+        clearModal();
     })
 })
